5f47f3d6b8f19d27db215a6b7fecdc68fd8e4cad
Jhalaaum <137584056+Jhalaaum@users.noreply.github.com>
2025-10-02 22:55:30 +0800

    Initial Commit

---
+++ Flute Translator/Flute_TranslatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Flute_TranslatorApp.swift
+//  Flute Translator
+//
+//  Created by Jhala family on 2/10/25.
+//
+
+import SwiftUI
+
+@main
+struct Flute_TranslatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
